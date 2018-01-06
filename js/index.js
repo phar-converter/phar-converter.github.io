ddbb8ac235edf6bb09c45f4526f52a3487359ef4
Serge Shkurko <shkurko1998@gmail.com>
2018-01-06 14:02:19 +0500

    Add support for file drag&drop (#1)

--- js/index.js
+++ js/index.js
@@ -10,9 +10,32 @@ function hideElements() {
 	}
 }
 
-window.onload = function() {
+document.addEventListener('DOMContentLoaded', function () {
+	var dragZone = document.querySelector('.upload')
+	
+	function drag(event) {
+		event.preventDefault();
+		dragZone.classList.add('upload--active');
+	}
+	
+	function dragEnd(event) {
+		event.preventDefault();
+		dragZone.classList.remove('upload--active');
+	}
+	
+	document.addEventListener('dragover', drag, false);
+	document.addEventListener('dragenter', drag, false);
+	
+	document.addEventListener('dragleave', dragEnd, false);
+	document.addEventListener('dragend', dragEnd, false);
+	
+	document.addEventListener('drop', function (event) {
+		dragEnd(event);
+		processFile(event.dataTransfer);
+	}, false);
+	
 	showElements('upload');
-};
+});
 
 function processFile(elm) {
 	hideElements('converted', 'error.corrupted', 'error.file-format', 'error.reading');
@@ -22,13 +45,15 @@ function processFile(elm) {
 	var reader = new FileReader();
 	
 	if (filename.substring(filename.length - 5, filename.length) == '.phar') {
-		reader.onload = (function(file) {
-			return function(e) {
+		reader.onload = (function (file) {
+			return function (e) {
 				try {
 					var data = PharUtils.PharZipConverter.toZip(new PharUtils.Phar().loadPharData(new Uint8Array(e.target.result))).compress();
 					var filename = file.name;
 					filename = filename.substring(0, filename.length - 5) + '.zip';
-					saveAs(new Blob([data], {type: "application/zip"}), filename);
+					saveAs(new Blob([data], {
+						type: 'application/zip'
+					}), filename);
 					showElements('converted');
 				} catch (error) {
 					showElements('error.corrupted');
@@ -37,13 +62,15 @@ function processFile(elm) {
 			};
 		})(file);
 	} else if (filename.substring(filename.length - 4, filename.length) == '.zip') {
-		reader.onload = (function(file) {
-			return function(e) {
+		reader.onload = (function (file) {
+			return function (e) {
 				try {
 					var data = PharUtils.PharZipConverter.toPhar(new Uint8Array(e.target.result)).savePharData(true);
 					var filename = file.name;
 					filename = filename.substring(0, filename.length - 4) + '.phar';
-					saveAs(new Blob([data], {type: 'application/x-php'}), filename);
+					saveAs(new Blob([data], {
+						type: 'application/x-php'
+					}), filename);
 					showElements('converted');
 				} catch (error) {
 					showElements('error.corrupted');
@@ -57,10 +84,10 @@ function processFile(elm) {
 		return;
 	}
 	
-	reader.onerror = function(e) {
+	reader.onerror = function (e) {
 		showElements('error.reading');
 	};
-	reader.onloadend = function(e) {
+	reader.onloadend = function (e) {
 		showElements('upload');
 		hideElements('converting');
 	};
